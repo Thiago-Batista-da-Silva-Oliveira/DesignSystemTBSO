9a2e062693ecf9e579579b3cdaf0c9e694a583e2
Thiago <gmoliveira69@gmail.com>
2023-07-18 20:45:12 -0300

    docs: Alert

---
+++ src/stories/Alert.stories.ts
@@ -0,0 +1,61 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Alert } from "../components";
+
+const meta: Meta<typeof Alert> = {
+  title: "Custom Alert",
+  component: Alert,
+  argTypes: {
+    title: {
+      type: "string",
+    },
+    description: {
+      type: "string",
+    },
+    type: {
+      type: "string",
+    },
+    width: {
+        type: "string",
+    }
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Alert>;
+
+export const Success: Story = {
+  args: {
+    title: "Success",
+    description: "check it out",
+    type: "success",
+  },
+};
+
+
+export const Error: Story = {
+    args: {
+      title: "Error",
+      description: "check it out",
+      type: "error",
+    },
+  };
+
+  
+  export const Warning: Story = {
+    args: {
+      title: "Warning",
+      description: "check it out",
+      type: "warning",
+    },
+  };
+
+  
+  export const Info: Story = {
+    args: {
+      title: "Info",
+      description: "check it out",
+      type: "info",
+    },
+  };
+  
